d3d607e5645e3fa8feae8fa92e8eb07a6676bb70
Selena4 <43804966+Selena4@users.noreply.github.com>
2019-05-19 13:05:42 +0000

    Create unsp.py

---
+++ unsp.py
@@ -0,0 +1,29 @@
+import sys
+
+def check(a):
+	bool = False
+	for l in a:
+		if l not in alphabet:
+			continue
+		else:
+			bool = True
+			break
+	return bool	
+
+alphabet = []
+for _ in range(97,123):
+	alphabet.append(chr(_))
+
+text = open(sys.argv[1],'r')
+array = text.readlines()
+new_array = []
+for _ in array:
+	if check(_) == True:
+		new_array.append(_) 
+text.close()
+text = open(sys.argv[1],'w')
+string = ''
+for _ in new_array:
+	string = string + _
+text.write(string)
+text.close()
